260dba9f0108ac76502a953952d30fd8d4dec323
unknown <jlccx@live.com>
2017-12-25 23:59:22 -0400

    adding new content

---
+++ Dockerfile
@@ -0,0 +1,71 @@
+withCredentials([
+  string(credentialsId: 'GitHub-Personal-TokenAccess-JlccX', variable: 'GITHUB_TOKEN')
+]) {
+  env.GITHUB_TOKEN="$GITHUB_TOKEN"
+}
+
+node('master') {
+
+      sh '''
+        set +x
+        printf "The NODE_NAME parameter is: ${NODE_NAME}\n"
+        printf "The GIT_URL value is: ${GIT_URL}\n"
+        printf "The GIT_BRANCH value is: ${GIT_BRANCH}\n"
+        printf "\nThe system properties are:\n"
+        uname -a
+      '''
+
+    stage('pre-deploy'){
+
+      docker.withRegistry("https://hub.docker.com"){
+      docker.image("jlccxincontact/nodejs:alpine").inside("-u root:root"){
+
+        sh '''
+            set +x
+            printf "executing the pre-deploy stage.\n"
+        '''
+
+        //dir("$SOURCE_CODE_FOLDER"){
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'JlccX-SSH-Private-Key', url: 'git@github.com:JlccX/control-system.git']]])
+        //}
+
+        sh '''
+            set +x
+            printf "The downloaded code is:\n"
+            ls
+        '''
+
+        //stash name: "$SOURCE_CODE_FOLDER", includes: "$SOURCE_CODE_FOLDER/*"
+
+      }
+    }
+  }
+
+  stage("sonarqube"){
+    docker.withRegistry("https://hub.docker.com"){
+      docker.image("jlccxincontact/nodejs:alpine").inside("-u root:root"){
+          sh '''
+              set +x
+              printf "SonarQube stage.\n"
+              printf "The operating system properties are:\n"
+              uname -a
+          '''
+        }
+    }
+  }
+
+  
+  stage("deploy-tests"){
+    docker.withRegistry("https://hub.docker.com"){
+      docker.image("jlccxincontact/nodejs:alpine").inside("-u root:root"){
+          sh '''
+              set +x
+              printf "\nThe deploy test execution was canceled.\n"
+          '''
+        }
+    }
+  }
+
+}
+  
+
--- app - copia.js
+++ app - copia.js
@@ -15,10 +15,44 @@ db.serialize(function(){
 
     //db.run("CREATE TABLE [IF NOT EXISTS] members(memberId INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, mobile_phone TEXT NOT NULL) [WITHOUT ROWID]");
     //db.run("DROP TABLE IF EXISTS Members");
-    db.run("CREATE TABLE IF NOT EXISTS Members (Id INTEGER PRIMARY KEY, Firstname TEXT, Lastname TEXT, SSN TEXT, Phone TEXT, Cellphone TEXT, Email TEXT, Address TEXT, RegisterDate TEXT)");
-    //db.close();
+    db.run("CREATE TABLE IF NOT EXISTS Members (Id INTEGER, Firstname TEXT, Lastname TEXT, SSN TEXT, Phone TEXT, Cellphone TEXT, Email TEXT, Address TEXT, RegisterDate TEXT)");
+
+    var count = 0;
+    
+    var query = "SELECT Count(*) AS Count From Members";
+    
+    db.get(query, function(error, row){
+        if(error){
+            console.log("There is an error on the query -> "+JSON.stringify(error));
+            return error.message;
+        }
+        else{
+            count = row.Count;
+            console.log("The query response is: "+JSON.stringify(row));
+            console.log("The count recovered is: "+count);
+
+            if(count===0){
+
+                console.log("There are no data, new rows will be added.");
+
+                db.run("INSERT INTO Members (MemberId,Firstname,Lastname,Mobilephone) VALUES(?,?,?,?)",['1024','Juan','Chavez','71787735']);
+                db.run("INSERT INTO Members (MemberId,Firstname,Lastname,Mobilephone) VALUES(?,?,?,?)",['2048','Susana','Mendez','71787736']);
+                db.run("INSERT INTO Members (MemberId,Firstname,Lastname,Mobilephone) VALUES(?,?,?,?)",['4096','Paola','Arancibia','71787737']);
+                db.run("INSERT INTO Members (MemberId,Firstname,Lastname,Mobilephone) VALUES(?,?,?,?)",['8192','Zara','Perez','71787738']);
+                db.run("INSERT INTO Members (MemberId,Firstname,Lastname,Mobilephone) VALUES(?,?,?,?)",['16486','Ximena','Arellano','71787739']);
+
+               
+            }
+        }
+    });
 });
-db.close();
+
+//db.close();
+
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+
+var cookieData;
+var githubAccessToken;
 
 var app = express();
 
@@ -39,6 +73,7 @@ app.use(bodyParser.json());
 // app.use(passport.session());
 app.use(express.static(__dirname));
 
+
 app.get('/admin/member', function(req, res){
 
     console.log("Starting the member page.");
@@ -50,39 +85,28 @@ app.get('/admin/member', function(req, res){
         console.log("The member information will be updated.");
         let db = new sqlite.Database(dbFileName);
         //var query = "SELECT MemberId, Firstname, Lastname, Mobilephone WHERE MemberId = ?";
-        var query = "SELECT Id, Firstname, Lastname, SSN, Phone, Cellphone, Email, Address, RegisterDate From Members WHERE Id = ?";
+        var query = "SELECT MemberId, Firstname, Lastname From Members WHERE MemberId = ?";
         db.get(query, [memberId], function(error, row){
             if(error){
                 console.log("There is an error on the query -> "+JSON.stringify(error));
                 return error.message;
             }
             else{
-                var member = { 
-                    Id: row.Id, 
-                    Firstname: row.Firstname, 
-                    Lastname: row.Lastname,
-                    SSN: row.SSN,
-                    Phone: row.Phone,
-                    Cellphone: row.Cellphone,
-                    Email: row.Email,
-                    Address: row.Address,
-                    RegisterDate: row.RegisterDate
-                };
+                var member = { MemberId: row.MemberId, Firstname: row.Firstname, Lastname: row.Lastname };
                 console.log("The member xxx recovered is: "+JSON.stringify(row));
-                if(row.Id && row.Id>0){
+                if(row.MemberId && row.MemberId>0){
                     res.render("pages/member", { member: member, Title: "Member Edition" });
                 }
                 else
                 {
                     res.render("pages/member", { member: member, Title: "Create a new Member" });
                 }
-                db.close();
             }
         });
     }
     else{
         console.log("A new member wil be created.");
-        var member = { Id: 0, Firstname: '', Lastname: "", SSN: '', Phone: "", Cellphone: "", Email: "", Address: "", RegisterDate:"" };
+        var member = { MemberId: "", Firstname: "", Lastname: "" };
         res.render("pages/member", { member: member, Title: "Create a new Member" });
     }
 });
@@ -91,33 +115,35 @@ app.post('/admin/delete', function(req, res){
     
         console.log("Starting the delete event."+JSON.stringify(req.body));
     
-        var memberId = req.body.id;
+        var memberId = req.body.data;
 
         //memberId = 1024;
 
         console.log("The member id received is: "+memberId);
         let db = new sqlite.Database(dbFileName);
         //var query = "SELECT MemberId, Firstname, Lastname, Mobilephone WHERE MemberId = ?";
-        var query = "DELETE FROM Members WHERE Id = ?";
+        var query = "DELETE FROM Members WHERE MemberId = ?";
         db.get(query, [memberId], function(error, row){
             if(error){
                 console.log("There is an error on the query -> "+JSON.stringify(error));
                 return error.message;
             }
             else{
-                console.log("The member to remove is: "+JSON.stringify(row));
-                
-                getMembersList();
+                var member = { MemberId: row.MemberId, Firstname: row.Firstname, Lastname: row.Lastname };
+                console.log("The member xxx recovered is: "+JSON.stringify(row));
+                if(row.MemberId && row.MemberId>0){
+                    res.render("pages/member", { member: member, Title: "Member Edition" });
+                }
+                else
+                {
+                    res.render("pages/member", { member: member, Title: "New Member" });
+                }
             }
         });
         
     });
 
 app.get('/admin/members-list', function(req, res){
-    getMembersList();
-});
-
-function getMembersList(){
     console.log("Starting the members-list page.");
     // res.render(__dirname+"/views/members-list.html");
 
@@ -134,7 +160,7 @@ function getMembersList(){
         res.render("pages/members-list",{ Members: data, Title: title });
         db.close();
     });
-}
+});
 
 app.post('/admin/member', function(req, res){
     console.log("Redirecting to the /admin/members-list page.");
@@ -143,34 +169,23 @@ app.post('/admin/member', function(req, res){
     console.log("The lastname is: "+req.body.lastname);
     console.log("The memberId is: "+req.body.memberId);
 
-    var id = req.body.id;
-
-    if(id<=0){
-        id = null;
-    }
-
-    var data = [req.body.firstname, req.body.lastname, req.body.ssn, req.body.phone, req.body.cellphone, req.body.email, req.body.address, req.body.registerDate, id];
-    let db = new sqlite.Database(dbFileName);
+    var data = [req.body.firstname, req.body.lastname, req.body.memberId];
     //var data = [req.body.memberId];
 
-    if(req.body.id>0){
-        var query = "UPDATE Members SET Firstname = ?, Lastname = ?, SSN = ?, Phone = ?, Cellphone = ?, Email = ?, Address = ?, RegisterDate = ?  WHERE Id = ?";
-        
-        db.run(query, data, function(error, row){
-            if(error){
-                console.log("There is an error on the query -> "+JSON.stringify(error));
-                return error.message;
-            }
-            else{
-                console.log("The memberId updated is: "+req.body.memberId);
-                res.redirect("/admin/members-list?id=xyz1");
-            }
-        });
-    }
-    else{
-        db.run("INSERT INTO Members (Firstname,Lastname,SSN,Phone,Cellphone,Email,Address,RegisterDate,Id) VALUES(?,?,?,?,?,?,?,?,?)", data);
-        res.redirect("/admin/members-list?id=newMember");
-    }
+    var query = "UPDATE Members SET Firstname = ?, Lastname = ?  WHERE MemberId = ?";
+    
+    db.run(query, data, function(error, row){
+        if(error){
+            console.log("There is an error on the query -> "+JSON.stringify(error));
+            return error.message;
+        }
+        else{
+            
+            console.log("The memberId updated is: "+req.body.memberId);
+            res.redirect("/admin/members-list?id=xyz1");
+        }
+    });
+    
 });
 
 // var myTestCallbackFunction = function(data){
@@ -189,11 +204,11 @@ function getAllMembers(callback){
     var db = new sqlite.Database(dbFileName);
     var list = [];
 
-    db.all("Select Id, Firstname, Lastname, SSN, Phone, Cellphone, Email, Address, RegisterDate FROM Members ORDER BY Firstname, Lastname ", function(error, rows){
-        rows.forEach(function(row){
+    db.all("Select MemberId, Firstname, Lastname, Mobilephone FROM Members ORDER BY MemberId", function(error, rows){
+        rows.forEach(function(row) {
             //console.log(row.memberId, row.first_name, row.last_name, row.mobile_phone);
-            var member = {"Id": row.Id, "SSN":row.SSN, "Firstname":row.Firstname, "Lastname":row.Lastname, "Cellphone": row.Cellphone};
-            console.log("getAllMembers -> " + JSON.stringify(member));
+            var member = {"MemberId": row.MemberId, "Firstname":row.Firstname, "Lastname":row.Lastname, "Mobilephone":row.Mobilephone};
+            console.log("getAllMembers -> " + JSON.stringify(member) );
             list.push(member);
         });
         console.log("The returned list size is: "+list.length);
